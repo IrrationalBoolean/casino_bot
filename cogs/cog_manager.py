e85737503e92ecdc4cc2330c51de28b2557416ff
KJ <irateboolean@gmx.com>
2021-06-22 22:35:33 -0400

    added cogs/cog_manager

---
+++ cogs/cog_manager.py
@@ -0,0 +1,35 @@
+from discord.ext import commands
+
+
+class CogCog(commands.Cog, name="Cog Controls"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def load_cog(self, extension):
+        self.bot.load_extension(f"cogs.{extension}")
+
+    def unload_cog(self, extension):
+        if extension == "cog_manager":
+            return
+        self.bot.unload_extension(f"cogs.{extension}")
+
+    @commands.command(name="reload", help="reloads cog")
+    async def reload(self, ctx, extension):
+        self.unload_cog(extension)
+        msg = await ctx.reply(f"{extension} has been unloaded")
+        self.load_cog(extension)
+        await msg.edit(content=f"{extension} has been reloaded")
+
+    @commands.command(name="load", help="loads cog")
+    async def load(self, ctx, extension):
+        self.load_cog(extension)
+        await ctx.reply(f"{extension} has been loaded")
+
+    @commands.command(name="unload", help="unloads cog")
+    async def reload(self, ctx, extension):
+        self.unload_cog(extension)
+        await ctx.reply(f"{extension} has been unloaded")
+
+
+def setup(bot):
+    bot.add_cog(CogCog(bot))
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from dotenv import dotenv_values
 dotenv = dotenv_values(".env")
 TOKEN = dotenv["TOKEN"]
 bot = commands.Bot(command_prefix="~")
+bot.load_extension("cogs.cog_manager")
 
 bot.run(TOKEN)
