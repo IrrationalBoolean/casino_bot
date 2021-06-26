36f647a3f0ee4dbfcd6188ad608ff8f461205817
KJ <irateboolean@gmx.com>
2021-06-26 14:09:26 -0400

    updated db_manager

--- cogs/db_manager.py
+++ cogs/db_manager.py
@@ -1,11 +1,10 @@
 from discord.ext import commands
-
-# REQUIRED FOR DB
 import discord
 import aiosqlite
 
 
 async def connect_to_db(bot):
+    """called at bot on_ready, creates bot.db connection to casino_data.db"""
     bot.db = await aiosqlite.connect("casino_data.db")
     await bot.db.execute(
         "CREATE TABLE IF NOT EXISTS user (guild_id int, user_id int, balance int, "
@@ -19,17 +18,17 @@ async def adjust_balance_earned(bot, amount, user_id, guild_id):
     else:
         win = 0
         loss = amount
-    cursor = await bot.db.execute("INSERT OR IGNORE INTO user(guild_id, user_id,"
-                                  " balance, total_earnings, total_losses)"
-                                  " VALUES (?,?,?,?,?)",
+    cursor = await bot.db.execute("INSERT OR IGNORE INTO user(guild_id, user_id, "
+                                  "balance, total_earnings, total_losses) "
+                                  "VALUES (?,?,?,?,?)",
                                   (guild_id, user_id, amount, win, loss))
     if cursor.rowcount == 0:
         if win:
-            await bot.db.execute(f"UPDATE user SET balance = balance + ?, total_earnings = total_earnings + ?"
+            await bot.db.execute(f"UPDATE user SET balance = balance + ?, total_earnings = total_earnings + ? "
                                  "WHERE guild_id = ? and user_id = ?",
                                  (amount, amount, guild_id, user_id))
         else:
-            await bot.db.execute(f"UPDATE user SET balance = balance + ?, total_losses = total_losses + ?"
+            await bot.db.execute(f"UPDATE user SET balance = balance + ?, total_losses = total_losses + ? "
                                  "WHERE guild_id = ? and user_id = ?",
                                  (amount, amount, guild_id, user_id))
     cur = await bot.db.execute("SELECT balance, total_earnings, total_losses FROM user "
@@ -41,9 +40,9 @@ async def adjust_balance_earned(bot, amount, user_id, guild_id):
 
 
 async def adjust_balance(bot, amount, user_id, guild_id):
-    cursor = await bot.db.execute("INSERT OR IGNORE INTO user(guild_id, user_id,"
-                                  " balance, total_earnings, total_losses)"
-                                  " VALUES (?,?,?,?,?)",
+    cursor = await bot.db.execute("INSERT OR IGNORE INTO user(guild_id, user_id, "
+                                  "balance, total_earnings, total_losses) "
+                                  "VALUES (?,?,?,?,?)",
                                   (guild_id, user_id, amount, 0, 0))
     if cursor.rowcount == 0:
         await bot.db.execute("UPDATE user SET balance = balance + ? WHERE guild_id = ? and user_id = ?",
@@ -51,10 +50,10 @@ async def adjust_balance(bot, amount, user_id, guild_id):
     await bot.db.commit()
 
 
-async def get_balance(bot, member):
-    cursor = await bot.db.execute(
-        "SELECT balance, total_earnings, total_losses  FROM user WHERE guild_id = ? and user_id =?",
-        (member.guild.id, member.author.id))
+async def get_balance(bot, user_id, guild_id):
+    cursor = await bot.db.execute("SELECT balance, total_earnings, total_losses "
+                                  "FROM user WHERE guild_id = ? and user_id =?",
+        (guild_id, user_id))
     bal = await cursor.fetchone()
     if bal:
         return bal
@@ -75,7 +74,6 @@ class DatabaseCog(commands.Cog, name="DatabaseCog"):
     @commands.command()
     async def earn(self, ctx, amount):
         await adjust_balance_earned(self.bot, int(amount), ctx.author.id, ctx.guild.id)
-        pass
 
     @commands.command()
     async def get(self, ctx, amount):
@@ -83,11 +81,11 @@ class DatabaseCog(commands.Cog, name="DatabaseCog"):
 
     @commands.command()
     async def bal(self, ctx):
-        bal = await get_balance(self.bot, ctx)
+        bal = await get_balance(self.bot, ctx.author.id, ctx.guild.id)
         if bal:
             await ctx.channel.send(f"Current balance is {bal[0]}, lifetime earn: {bal[1]}, lifetime loss: {bal[2]}")
         else:
-            await ctx.channel.send("You have no balance history.")
+            await ctx.channel.send("You have no balance history in this guild.")
 
     async def take(self):
         pass
