6eeba34190a817367c4a440a5fe4f8a314e3b123
LeATeP <74774288+LeATeP@users.noreply.github.com>
2021-06-27 00:43:39 +0400

    Update cog_manager.py
    advanced toss a coin

--- cogs/cog_manager.py
+++ cogs/cog_manager.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from random import randint
+from cogs.db_manager import adjust_balance_earned
 
 
 class CogCog(commands.Cog, name="Cog Controls"):
@@ -32,11 +33,18 @@ class CogCog(commands.Cog, name="Cog Controls"):
         await ctx.reply(f"{extension} has been unloaded")
 
     @commands.command(name="coin", help="to toss a coin 0 or 1")
-    async def toss_coin(self, ctx, extension=1):
-        coin = randint(0, extension)
-        if extension > 1:
-            coin + 1
-        await ctx.reply(coin)
+    async def toss_coin(self, ctx, player_roll: int, roll_max: int = 1, bet: int = 0):
+        coin_roll = randint(0, roll_max)
+        if roll_max > 1:
+            coin_roll + 1
+        if player_roll == coin_roll and bet > 0:
+            bet = bet * 2
+            await adjust_balance_earned(self.bot, bet, ctx.author.id, ctx.guild.id)
+            await ctx.reply(f'{coin_roll} You won {bet}')
+        else:
+            bet = bet * -1
+            await adjust_balance_earned(self.bot, bet, ctx.author.id, ctx.guild.id)
+            await ctx.reply(f'{coin_roll} You lose {bet}')
 
 def setup(bot):
     bot.add_cog(CogCog(bot))
