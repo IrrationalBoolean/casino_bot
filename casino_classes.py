b13083e98f0b352046d3d933029f1e77e70c148f
=GV= <77139649+anon-legion@users.noreply.github.com>
2021-06-28 09:17:46 +0800

    commit casino_classes.py for comment

--- casino_classes.py
+++ casino_classes.py
@@ -1,16 +1,22 @@
 # Krzysztof requesting for comment regarding the direction I am taking before I proceed any further
-cardsInDeck = [suit + str(val) for suit in 'cdhs' for val in [i for i in range(2, 11)] + ['j', 'q', 'k', 'a']]
+cardsInDeck = [suit + str(val) for suit in 'CDHS' for val in [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']]
 
+#should I just remove self._handSize from __init__ and just keep a getHandSize method that only returns the length of self._cards?
 class Hand(object):
     def __init__(self):
-        self._cards = {}
+        self._cards = []
         self._handSize = len(self._cards)
 
-    def getCards(self, imageOnly=False):
-        assert type(imageOnly) == bool
+    def __len__(self):
+        return len(self.getCards())
+
+    def getCards(self, isImageOnly=False, isKeyOnly=False):
+        assert type(isImageOnly) == bool and not (isImageOnly and isKeyOnly)
         try:
-            if imageOnly:
-                return self._cards.values()
+            if isImageOnly:
+                return [list(card.values())[0] for card in self._cards]
+            elif isKeyOnly:
+                return [list(card.keys())[0] for card in self._cards]
             else:
                 return self._cards
         except AssertionError:
@@ -23,14 +29,19 @@ class Hand(object):
     def setCards(self, cards):
         # create assertion for cards
         self._cards = cards
+        self.setHandSize()
+
+    def setHandSize(self):
+        self._handSize = len(self.getCards())
 
     def receiveCard(self, card):
         # create assertion for card
-        self._cards.update(card)
+        self._cards.append(card)
+        self.setHandSize()
 
-    def discard(self, cardChoice = None):
+    def discard(self, cardChoice=None):
         """
-        discards a card from hand, to discard entire hand perform while self.getHandize > 0 self.discard()
+        discards a card from hand
 
         cardChoice: string (optional)
             assumes a string in cardsInDeck
@@ -41,22 +52,81 @@ class Hand(object):
         assert self.getHandSize() > 0
         try:
             if cardChoice is None:
-                return {list(self.getCards())[-1]: self._cards.pop(list(self.getCards())[-1])}
+                toDiscard = self._cards.pop()
             else:
-                if cardChoice.lower() in self.getCards():
-                    return {cardChoice.lower(): self._cards.pop(cardChoice)}
-                else:
-                    raise AssertionError
-        except AssertionError:
+                indexOfCard = self.getCards(isKeyOnly=True).index(cardChoice.upper())
+                toDiscard = self._cards.pop(indexOfCard)
+            self.setHandSize()
+            return toDiscard
+        except (AssertionError, ValueError) as err:  # ValueError if .index() does not find cardChoice in self._hand
             print('Invalid move!')
             # invalid move operation
 
+    def discardHand(self):
+        while self.getHandSize() > 0:
+            self.discard()
+        self.setHandSize()
+
 
 class Player(object):
-    def __init__(self, buyIn):
-        self.balance = buyIn
-        self.hand = Hand()
+    def __init__(self, buyIn, seatNum, isDealer=False):
+        self._hand = Hand()
+        self._balance = buyIn
+        self._seatNum = seatNum
+        self._isDealer = isDealer
 
-    pass
+    def getHand(self):
+        return self._hand.getCards(isImageOnly=True)
 
+    def getBalance(self):
+        return self._balance
 
+    def getSeatNum(self):
+        return self._seatNum
+
+    def getIsDealer(self):
+        return self._isDealer
+
+    def setHand(self, cards):
+        self._hand.setCards(cards)
+
+    def setBalance(self, amount):
+        self._balance = amount
+
+    def setSeatNum(self, num):
+        self._seatNum = num
+
+    def setIsDealer(self):
+        if self.getIsDealer():
+            self._isDealer = False
+        else:
+            self._isDealer = True
+
+    def check(self):
+        pass
+
+    def bet(self, amount):
+        assert type(amount) == int and (0 < amount <= self.getBalance())
+        try:
+            self.setBalance(self.getBalance() - amount)
+            return amount
+        except AssertionError:
+            print('Invalid!')
+            # invalid bet operation
+
+    def call(self, toCall):
+        assert type(toCall) == int
+        try:
+            if toCall > self.getBalance():
+                self.allIn()
+            else:
+                self.bet(toCall)
+        except AssertionError:
+            print('Invalid')
+            # invalid toCall
+
+    def allIn(self):
+        self.bet(self.getBalance())
+
+    def fold(self):
+        self._hand.discardHand()
