baef3efeb1554b228d0aa4d0c935199b760614f6
=GV= <77139649+anon-legion@users.noreply.github.com>
2021-06-29 14:26:36 +0800

    update preliminary casino_classes.py with property decorators for testing

--- casino_classes.py
+++ casino_classes.py
@@ -1,43 +1,45 @@
 # Krzysztof requesting for comment regarding the direction I am taking before I proceed any further
 cardsInDeck = [suit + str(val) for suit in 'CDHS' for val in [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']]
 
-#should I just remove self._handSize from __init__ and just keep a getHandSize method that only returns the length of self._cards?
+
 class Hand(object):
     def __init__(self):
         self._cards = []
-        self._handSize = len(self._cards)
 
     def __len__(self):
-        return len(self.getCards())
+        return len(self.cards)
 
-    def getCards(self, isImageOnly=False, isKeyOnly=False):
-        assert type(isImageOnly) == bool and not (isImageOnly and isKeyOnly)
+    def __iadd__(self, card):
+        if type(card) == list:
+            self.cards += card
+        else:
+            self.cards += [card]
+        return self
+
+    @property
+    def cards(self):
+        return self.getCards()
+
+    @cards.setter
+    def cards(self, cards):
+        # create assertion for cards
+        if type(cards) == list:
+            self._cards = cards
+        else:
+            self._cards = [cards]
+
+    def getCards(self, isKeyOnly: bool = False, isImageOnly: bool = False) -> list:
+        assert (type(isImageOnly) == bool and type(isKeyOnly) == bool) and not (isImageOnly and isKeyOnly)
         try:
             if isImageOnly:
-                return [list(card.values())[0] for card in self._cards]
+                return [list(card.values())[0] for card in self._cards][:]
             elif isKeyOnly:
-                return [list(card.keys())[0] for card in self._cards]
+                return [list(card.keys())[0] for card in self._cards][:]
             else:
-                return self._cards
+                return self._cards[:]
         except AssertionError:
             pass
             # invalid request operation
-
-    def getHandSize(self):
-        return self._handSize
-
-    def setCards(self, cards):
-        # create assertion for cards
-        self._cards = cards
-        self.setHandSize()
-
-    def setHandSize(self):
-        self._handSize = len(self.getCards())
-
-    def receiveCard(self, card):
-        # create assertion for card
-        self._cards.append(card)
-        self.setHandSize()
 
     def discard(self, cardChoice=None):
         """
@@ -49,75 +51,77 @@ class Hand(object):
         return: dictionary
             discarded card
         """
-        assert self.getHandSize() > 0
+        assert len(self) > 0
         try:
             if cardChoice is None:
                 toDiscard = self._cards.pop()
             else:
                 indexOfCard = self.getCards(isKeyOnly=True).index(cardChoice.upper())
                 toDiscard = self._cards.pop(indexOfCard)
-            self.setHandSize()
             return toDiscard
-        except (AssertionError, ValueError) as err:  # ValueError if .index() does not find cardChoice in self._hand
+        except (AssertionError, ValueError) as err:  # ValueError if .index() does not find cardChoice in self._cards
             print('Invalid move!')
             # invalid move operation
 
     def discardHand(self):
-        while self.getHandSize() > 0:
+        while len(self) > 0:
             self.discard()
-        self.setHandSize()
 
 
 class Player(object):
     def __init__(self, buyIn, seatNum, isDealer=False):
-        self._hand = Hand()
         self._balance = buyIn
         self._seatNum = seatNum
         self._isDealer = isDealer
 
-    def getHand(self):
-        return self._hand.getCards(isImageOnly=True)
+    def __iadd__(self, amount):
+        self.balance = self.balance + amount
+        return self
 
-    def getBalance(self):
+    def __isub__(self, amount):
+        self.balance = self.balance - amount
+        return self
+
+    @property
+    def balance(self):
         return self._balance
 
-    def getSeatNum(self):
+    @property
+    def seatNum(self):
         return self._seatNum
 
-    def getIsDealer(self):
+    @property
+    def isDealer(self):
         return self._isDealer
 
-    def setHand(self, cards):
-        self._hand.setCards(cards)
-
-    def setBalance(self, amount):
+    @balance.setter
+    def balance(self, amount):
         self._balance = amount
 
-    def setSeatNum(self, num):
+    @seatNum.setter
+    def seatNum(self, num):
         self._seatNum = num
 
-    def setIsDealer(self):
-        if self.getIsDealer():
-            self._isDealer = False
-        else:
-            self._isDealer = True
+    @isDealer.setter
+    def isDealer(self, button: bool):
+        self._isDealer = button
 
     def check(self):
         pass
 
-    def bet(self, amount):
-        assert type(amount) == int and (0 < amount <= self.getBalance())
+    def bet(self, amount: int) -> int:
+        assert type(amount) == int and (0 < amount <= self.balance)
         try:
-            self.setBalance(self.getBalance() - amount)
+            self.balance -= amount
             return amount
         except AssertionError:
             print('Invalid!')
             # invalid bet operation
 
-    def call(self, toCall):
+    def call(self, toCall: int):
         assert type(toCall) == int
         try:
-            if toCall > self.getBalance():
+            if toCall > self.balance:
                 self.allIn()
             else:
                 self.bet(toCall)
@@ -126,7 +130,4 @@ class Player(object):
             # invalid toCall
 
     def allIn(self):
-        self.bet(self.getBalance())
-
-    def fold(self):
-        self._hand.discardHand()
+        self.bet(self.balance)
