b0526b354a143d090d511476d9eddbbc9375afb5
=GV= <77139649+anon-legion@users.noreply.github.com>
2021-06-30 21:42:12 +0800

    update casino_classes.py for consideration

--- casino_classes.py
+++ casino_classes.py
@@ -1,5 +1,5 @@
 # Krzysztof requesting for comment regarding the direction I am taking before I proceed any further
-cardsInDeck = [suit + str(val) for suit in 'CDHS' for val in [i for i in range(2, 11)] + ['J', 'Q', 'K', 'A']]
+cardsInDeck = [suit + str(val) for suit in 'CDHS' for val in [i for i in range(2, 10)] + [0] + ['J', 'Q', 'K', 'A']]
 
 
 class Hand(object):
@@ -16,9 +16,16 @@ class Hand(object):
             self.cards += [card]
         return self
 
+    def __iter__(self):
+        return iter(self._cards)
+
+    def __repr__(self):
+        return str(self.cards)
+
     @property
     def cards(self):
-        return self.getCards()
+        return self._cards[:]
+        # return self.getCards()
 
     @cards.setter
     def cards(self, cards):
@@ -28,20 +35,20 @@ class Hand(object):
         else:
             self._cards = [cards]
 
-    def getCards(self, isKeyOnly: bool = False, isImageOnly: bool = False) -> list:
-        assert (type(isImageOnly) == bool and type(isKeyOnly) == bool) and not (isImageOnly and isKeyOnly)
-        try:
-            if isImageOnly:
-                return [list(card.values())[0] for card in self._cards][:]
-            elif isKeyOnly:
-                return [list(card.keys())[0] for card in self._cards][:]
-            else:
-                return self._cards[:]
-        except AssertionError:
-            pass
-            # invalid request operation
+   # def getCards(self, isKeyOnly: bool = False, isImageOnly: bool = False) -> list:
+   #      assert (type(isImageOnly) == bool and type(isKeyOnly) == bool) and not (isImageOnly and isKeyOnly)
+   #      try:
+   #          if isImageOnly:
+   #              return [list(card.values())[0] for card in self._cards][:]
+   #          elif isKeyOnly:
+   #              return [list(card.keys())[0] for card in self._cards][:]
+   #          else:
+   #              return self._cards[:]
+   #      except AssertionError:
+   #          pass
+   #          # invalid request operation
 
-    def discard(self, cardChoice=None):
+    def discard(self, card_choice=None):
         """
         discards a card from hand
 
@@ -53,26 +60,26 @@ class Hand(object):
         """
         assert len(self) > 0
         try:
-            if cardChoice is None:
-                toDiscard = self._cards.pop()
+            if card_choice is None:
+                to_discard = self._cards.pop()
             else:
-                indexOfCard = self.getCards(isKeyOnly=True).index(cardChoice.upper())
-                toDiscard = self._cards.pop(indexOfCard)
-            return toDiscard
+                index_of_card = self.cards.index(card_choice.upper())
+                to_discard = self._cards.pop(index_of_card)
+            return to_discard
         except (AssertionError, ValueError) as err:  # ValueError if .index() does not find cardChoice in self._cards
             print('Invalid move!')
             # invalid move operation
 
-    def discardHand(self):
+    def discard_hand(self):
         while len(self) > 0:
             self.discard()
 
 
 class Player(object):
-    def __init__(self, buyIn, seatNum, isDealer=False):
-        self._balance = buyIn
-        self._seatNum = seatNum
-        self._isDealer = isDealer
+    def __init__(self, buyin, seatnum, is_dealer=False):
+        self._balance = buyin
+        self._seat = seatnum
+        self._is_dealer = is_dealer
 
     def __iadd__(self, amount):
         self.balance = self.balance + amount
@@ -82,29 +89,32 @@ class Player(object):
         self.balance = self.balance - amount
         return self
 
+    def __repr__(self):
+        return str(self.balance)
+
     @property
     def balance(self):
         return self._balance
 
     @property
-    def seatNum(self):
-        return self._seatNum
+    def seat(self):
+        return self._seat
 
     @property
-    def isDealer(self):
-        return self._isDealer
+    def is_dealer(self):
+        return self._is_dealer
 
     @balance.setter
     def balance(self, amount):
         self._balance = amount
 
-    @seatNum.setter
-    def seatNum(self, num):
-        self._seatNum = num
+    @seat.setter
+    def seat(self, num):
+        self._seat = num
 
-    @isDealer.setter
-    def isDealer(self, button: bool):
-        self._isDealer = button
+    @is_dealer.setter
+    def is_dealer(self, button: bool):
+        self._is_dealer = button
 
     def check(self):
         pass
@@ -118,16 +128,16 @@ class Player(object):
             print('Invalid!')
             # invalid bet operation
 
-    def call(self, toCall: int):
-        assert type(toCall) == int
+    def call(self, to_call: int):
+        assert type(to_call) == int
         try:
-            if toCall > self.balance:
-                self.allIn()
+            if to_call > self.balance:
+                self.all_in()
             else:
-                self.bet(toCall)
+                self.bet(to_call)
         except AssertionError:
             print('Invalid')
             # invalid toCall
 
-    def allIn(self):
+    def all_in(self):
         self.bet(self.balance)
