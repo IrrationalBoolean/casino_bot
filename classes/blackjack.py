a98870d22ff484d6ae452757e86783a1edec950a
KJ <irateboolean@gmx.com>
2021-06-30 14:12:09 -0400

    cleanup

--- classes/blackjack.py
+++ classes/blackjack.py
@@ -6,37 +6,35 @@ class BlackJackHand(Hand):
     def __init__(self):
         super().__init__()
 
-    def __iter__(self):
-        return iter(self._cards)
-
-    def __repr__(self):
-         return repr(self.cards)
-
     def __eq__(self, other):
         if not isinstance(other, BlackJackHand):
-            return TypeError("Can only compare BlackJackHand with another BlackJackHand")
+            return TypeError("Can only compare BlackJackHand with"
+                             " another BlackJackHand")
         return self.tabulate_score() == other.tabulate_score()
 
     def __ne__(self, other):
         if not isinstance(other, BlackJackHand):
-            return TypeError("Can only compare BlackJackHand with another BlackJackHand")
+            return TypeError("Can only compare BlackJackHand with"
+                             " another BlackJackHand")
         return self.tabulate_score() != other.tabulate_score()
 
     def __lt__(self, other):
         if not isinstance(other, BlackJackHand):
-            return TypeError("Can only compare BlackJackHand with another BlackJackHand")
+            return TypeError("Can only compare BlackJackHand with"
+                             " another BlackJackHand")
         return self.tabulate_score() < other.tabulate_score()
 
     def __gt__(self, other):
         if not isinstance(other, BlackJackHand):
-            return TypeError("Can only compare BlackJackHand with another BlackJackHand")
+            return TypeError("Can only compare BlackJackHand with"
+                             " another BlackJackHand")
         return self.tabulate_score() > other.tabulate_score()
 
+
     def tabulate_score(self):
-        if not self._cards:
+        if not self.cards:
             return 0
         aces = sum([1 for card in self if card[1] == "A"])
-
         score = sum([self.VALUES[card[-1]] for card in self])
         while score > 21 and aces:
             score -= 10
